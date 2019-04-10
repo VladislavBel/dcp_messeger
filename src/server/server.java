3ef23010b2904fa472d05294d6c0ae97924d5a1d
VladislavBel <vladbel1999@gmail.com>
2019-04-10 14:54:53 +0300

    Initial commit

---
+++ src/server/server.java
@@ -0,0 +1,11 @@
+package server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class server {
+
+}
